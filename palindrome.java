a3188119075e184a31851a23b93bb00cb684743c
Megan Kale <meganleannekale@gmail.com>
2019-12-27 18:04:00 -0500

    Add existing files

---
+++ palindrome.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+//length/2 = even length strings
+//length-1/2 = odd length strings
+
+
+class palindrome{
+
+	public static boolean isPal(String s){
+		char[] c = s.toCharArray();
+		for (int i = 0; i <= (s.length()-1)/2; i++){
+			if (c[i] != c[s.length()-1-i]){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter text: ");
+    	String s = input.next();
+		int n = 2; //min length of palindrome
+		for (int i = 0; i < (s.length() - 1); i++) {
+			System.out.println("i: " + i);
+			for (int j = i + n; j < (s.length() + 1); j++) {
+				System.out.println("j: " + j);
+				if (isPal(s.substring(i, j))) {
+					System.out.println(s.substring(i, j));
+				}
+			}
+		}
+		input.close();
+	}
+}
---
+++ sockMerch.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class sockMerch {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        int id = 0;
+        int numPairs= 0;
+        int[] color = new int[n];
+
+        for (int i = 0; i < n; i++){
+            for (int j = i+1; j < n; j++){
+                if (ar[i] == ar[j]){
+                color[id]++;}
+            }
+            id++;
+        }
+        for (int k = 0; k < id; k++){
+            if (color[k] > 0){
+                int num = color[k]%2;
+                numPairs += num;
+            }
+        }
+        return numPairs;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+		int[] ar = new int[]{ 10,20,30,30,20,40,10 };
+		int n = ar.length;
+		
+        int result = sockMerchant(n, ar);
+
+        System.out.println(String.valueOf("Number of Pairs of Socks: " + result));
+    }
+}
